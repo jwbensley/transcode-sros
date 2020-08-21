eefa2881b2ae6ab753148b2f7c0f8fd74b17ece0
jwbensley <jwbensley@gmail.com>
2020-08-21 11:29:01 +0100

    Added skipping of section headers

--- trancode-sros.py
+++ trancode-sros.py
@@ -27,70 +27,73 @@ exit_found = False
 for read in file_cfg :
     # Read an config line
     line = read
+
     # Print lines already in the long format (beinging with "/") verbatim
     if (re.search("^ *\/", line)):
         print(line.strip())
         continue
-    # Extract Comment lines
+    
+    # Skip lines which are comments, echo statements of section headers (only "-" character)
     isComment = re.search("^ *#", line)
     isEcho = re.search("^ *echo", line)
-    # Exclude comment, echo and empty line
-    if not isComment:
-        if not isEcho:
-            if line.strip()!='' and line.strip() != 'exit all':
-                #SROS indentation is made of 4 space characters
-                indentation = line.count('    ')            
-                #Manage line that are not made of an exit 
-                if line.strip() != 'exit':  
-                    #If current line is more specific that previous one add word to the list
-                    if ((indentation > last_indentation ) or indentation == 0 ):
-                        #Add the / for configure statement
-                        if line.strip() == 'configure':
-                            line = '/'+ line
-                        #manage line with create word differently
-                        if 'create' in line.strip():
-                            create_line = ' '.join(tab_LINE) + ' ' + line.strip()
-                            print(create_line)
-                            line = line.replace(' create','').strip()
-                        if index>0 and 'create' in tab_LINE[index-1]:
-                            print(' '.join(tab_LINE))
-                            tab_LINE[index-1] = tab_LINE[index-1].replace(' create','')
-                        tab_LINE.append(line.strip())
-                        index+=1
-                        exit_found = False
-                    #If current line is less specific that previous one remove word to the list
-                    if (indentation < last_indentation ):
-                        del tab_LINE[index-1]
-                        tab_LINE.append(line.strip())       
-                    #If current line is same specific that previous and depending on last exit found write config line in file
-                    if ((last_indentation == indentation ) and indentation!=0 ):
-                        if exit_found:                          
-                            del tab_LINE[index-1]
-                            tab_LINE.append(line.strip())
-                            print(' '.join(tab_LINE))
-                        else:
-                            print(' '.join(tab_LINE))
-                            del tab_LINE[index-1]
-                            tab_LINE.append(line.strip())
-                    last_indentation = indentation
-                    
-                        
-                #Manage line with an exit statement
+    isHeader = re.match("^-+$", line)
+    if isComment or isEcho or isHeader:
+        continue
+
+    if line.strip()!='' and line.strip() != 'exit all':
+        #SROS indentation is made of 4 space characters
+        indentation = line.count('    ')            
+        #Manage line that are not made of an exit 
+        if line.strip() != 'exit':  
+            #If current line is more specific that previous one add word to the list
+            if ((indentation > last_indentation ) or indentation == 0 ):
+                #Add the / for configure statement
+                if line.strip() == 'configure':
+                    line = '/'+ line
+                #manage line with create word differently
+                if 'create' in line.strip():
+                    create_line = ' '.join(tab_LINE) + ' ' + line.strip()
+                    print(create_line)
+                    line = line.replace(' create','').strip()
+                if index>0 and 'create' in tab_LINE[index-1]:
+                    print(' '.join(tab_LINE))
+                    tab_LINE[index-1] = tab_LINE[index-1].replace(' create','')
+                tab_LINE.append(line.strip())
+                index+=1
+                exit_found = False
+            #If current line is less specific that previous one remove word to the list
+            if (indentation < last_indentation ):
+                del tab_LINE[index-1]
+                tab_LINE.append(line.strip())       
+            #If current line is same specific that previous and depending on last exit found write config line in file
+            if ((last_indentation == indentation ) and indentation!=0 ):
+                if exit_found:                          
+                    del tab_LINE[index-1]
+                    tab_LINE.append(line.strip())
+                    print(' '.join(tab_LINE))
                 else:
-                    if exit_found != True:
-                        if last_indentation != indentation:
-                            print(' '.join(tab_LINE))
-                            del tab_LINE[index-1]
-                            index-=1
-                            del tab_LINE[index-1]
-                            index-=1
-                            last_indentation = indentation - 1
-                            exit_found = True                   
-                        else:
-                            exit_found = False
-                    else:
-                        del tab_LINE[index-1]
-                        index-=1    
-                        last_indentation = indentation - 1
+                    print(' '.join(tab_LINE))
+                    del tab_LINE[index-1]
+                    tab_LINE.append(line.strip())
+            last_indentation = indentation
+            
+                
+        #Manage line with an exit statement
+        else:
+            if exit_found != True:
+                if last_indentation != indentation:
+                    print(' '.join(tab_LINE))
+                    del tab_LINE[index-1]
+                    index-=1
+                    del tab_LINE[index-1]
+                    index-=1
+                    last_indentation = indentation - 1
+                    exit_found = True                   
+                else:
+                    exit_found = False
+            else:
+                del tab_LINE[index-1]
+                index-=1    
+                last_indentation = indentation - 1
                 
 sys.stdout.close ()
